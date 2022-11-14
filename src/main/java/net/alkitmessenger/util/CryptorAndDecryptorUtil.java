a7e429e002205cd24c868e74c820d006efc67332
Alterrutm <markpav383@gmail.com>
2022-11-14 12:30:15 +0300

    Update File cryptor

--- src/main/java/net/alkitmessenger/util/CryptorAndDecryptorUtil.java
+++ src/main/java/net/alkitmessenger/util/CryptorAndDecryptorUtil.java
@@ -2,10 +2,10 @@ package net.alkitmessenger.util;
 
 public class CryptorAndDecryptorUtil {
     public byte[] byteCryptor(byte[] buffer){
-
         for (int i = 0; i < buffer.length; i++) {
-            buffer[i] = (byte) -buffer[i];
+            buffer[i] = (byte) -buffer[i]; //меняем знак байта
         }
+        //меняем рассположение в массиве
         for (int i = 0; i < buffer.length / 2; i++) {
             int temp = buffer[i];
             buffer[i] = buffer[buffer.length - 1 - i];
@@ -13,5 +13,16 @@ public class CryptorAndDecryptorUtil {
         }
         return buffer;
     }
-//    public byte[] byteDecryptor
+    public byte[] byteDecryptor(byte[] buffer){
+        for (int i = 0; i < buffer.length; i++) {
+            buffer[i] = (byte) -buffer[i]; //меняем знак байта
+        }
+        //меняем рассположение в массиве
+        for (int i = 0; i < buffer.length / 2; i++) {
+            int temp = buffer[i];
+            buffer[i] = buffer[buffer.length - 1 - i];
+            buffer[buffer.length - 1 - i] = (byte) temp;
+        }
+        return buffer;
+    }
 }
