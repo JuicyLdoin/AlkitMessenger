a1f3cef6a1f9a31e918bdc387743adaf02563f79
juicyldoin <ldoinplay224@gmail.com>
2022-11-26 14:27:03 +0200

    update PacketFeedback

--- src/main/java/net/alkitmessenger/user/UserConnection.java
+++ src/main/java/net/alkitmessenger/user/UserConnection.java
@@ -78,8 +78,7 @@ public class UserConnection extends Thread {
                         feedback.setReceivedPacket(inputPacket);
                         feedback.resume(inputPacket instanceof ExceptionPacket ? PacketFeedback.Reason.EXCEPTION : PacketFeedback.Reason.PACKET);
 
-                        if (feedback.isRead())
-                            toRemove.add(feedback);
+                        toRemove.add(feedback);
 
                     });
 
